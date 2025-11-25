d3f8883967197edfa039595fff7623f44eac8cfd
mauro <maurogioe91@libero.it>
2025-11-25 16:38:36 +0100

    changed data splits

--- data_split.py
+++ data_split.py
@@ -4,7 +4,7 @@ import pandas as pd
 data = pd.read_csv("./data/creditcard.csv")
 
 data = data.drop(["Time"],axis=1)
-train=data.sample(frac=0.333,random_state=200)
+train=data.sample(frac=0.70,random_state=200)
 test_prod=data.drop(train.index)
 test = test_prod.sample(frac=0.50,random_state=200)
 prod=test_prod.drop(test.index)
