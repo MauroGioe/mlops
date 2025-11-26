2115a0dbc7cae716f549d9ab6b6f89194740879f
mauro <maurogioe91@libero.it>
2025-11-26 17:31:29 +0100

    first commit

---
+++ training_kubeflow.py
@@ -0,0 +1,101 @@
+import pandas as pd
+from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score,precision_score,recall_score
+from sklearn.inspection import permutation_importance
+import mlflow
+import pickle
+from time import gmtime, strftime
+import kfp
+from kfp import dsl
+from kfp.dsl import Dataset
+from typing import NamedTuple
+
+@dsl.component(base_image="python:3.11-slim",
+    packages_to_install=["pandas", "mlflow"])
+def load_data()->(
+        NamedTuple("Datasets", [("X_train", Dataset),("y_train", Dataset),("X_test", Dataset),("y_test", Dataset)])):
+    experiment_name = "Credit card fraud"
+    mlflow.set_experiment(experiment_name)
+
+    train = pd.read_csv("./data/train.csv")
+    test = pd.read_csv("./data/test.csv")
+    y_train = train["Class"]
+    X_train = train.drop("Class", axis=1)
+    y_test = test["Class"]
+    X_test = test.drop("Class", axis=1)
+    return (X_train, y_train, X_test, y_test)
+
+
+
+@dsl.component(base_image="python:3.11-slim",
+    packages_to_install=["pandas", "scikit-learn", "xgboost", "mlflow"]
+)
+def train_model(X_train:Dataset, y_train:Dataset, X_test:Dataset, y_test:Dataset):
+    scale_pos_weight = y_train.value_counts()[0]/y_train.value_counts()[1]
+    param_grid = {
+        'max_depth': [x for x in range(2,20)],
+        'learning_rate': [0.01, 0.5, 1],
+        'n_estimators': [10, 100, 200]
+    }
+    model = XGBClassifier(objective='binary:logistic', seed = 123, scale_pos_weight=scale_pos_weight)
+    grid_search = GridSearchCV(estimator = model, param_grid = param_grid, scoring ="f1", cv=3)
+
+    grid_search.fit(X_train, y_train)
+    best_model = grid_search.best_estimator_
+    best_params = grid_search.best_params_
+    with open("./models/xgb.pkl", "wb") as f:
+        pickle.dump(best_model, f)
+
+    results = pd.DataFrame(grid_search.cv_results_)
+    results = results[['params', 'mean_test_score', 'std_test_score']]
+    max_run = results["mean_test_score"].idxmax()
+    results = results.values
+    excution_time = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
+    for i,run in enumerate(results):
+        with mlflow.start_run(run_name=excution_time+"_cv_"+str(i)):
+            mlflow.set_tag("model", "xgboost")
+            # Log model configuration/params
+            mlflow.log_params(run[0])
+            # Log metrics
+            metrics = {
+                'f1-score': run[1],
+                'std': run[2]
+            }
+            mlflow.log_metrics(metrics)
+            if i == max_run:
+                mlflow.log_artifact("./models/xgb.pkl", artifact_path="models_pickle")
+                y_predict = best_model.predict(X_test)
+                f1 = f1_score(y_test, y_predict)
+                precision = precision_score(y_test, y_predict)
+                recall = recall_score(y_test, y_predict)
+                best_metrics = {"test f1-score":f1, "test precision":precision, "test recall":recall}
+                print(f"Best model test metrics: f-1 score {f1}, precision {precision}, recall {recall }")
+                mlflow.log_metrics(best_metrics)
+                mlflow.sklearn.log_model(sk_model=best_model, name="xgboost", input_example=X_train,
+                                         registered_model_name="xgboost")
+
+        mlflow.end_run()
+
+    perm_importance = permutation_importance(best_model, X_test, y_test, scoring = "f1")
+    sorted_idx = perm_importance.importances_mean.argsort()
+    list(zip(X_test.columns[sorted_idx], perm_importance.importances_mean[sorted_idx]))[::-1]
+    top_features = X_test.columns[sorted_idx][::-1][:4].to_list()
+    with open('top_features.pkl', 'wb') as f:
+        pickle.dump(top_features, f)
+
+@dsl.pipeline(
+    name="Credit-card-fraud-pipeline",
+    description="ML pipeline that creates ml flow model."
+)
+def pipeline():
+    data = load_data()
+    train_model(X_train=data.outputs["X_train"], y_train=data.outputs["y_train"], X_test=data.outputs["X_test"],
+                y_test=data.outputs["y_test"])
+
+
+if __name__ == "__main__":
+    kfp.compiler.Compiler().compile(
+        pipeline_func=pipeline,
+        package_path="pipeline.yaml"
+    )
